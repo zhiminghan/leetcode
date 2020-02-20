a2712cac300e397ed8c6186f32c2768430ea3c8b
xiaozhi <xiaozhi@163.com>
2020-02-20 21:47:51 +0800

    leetcode 658. 找到 K 个最接近的元素

--- src/main/java/com/alogrithm/leetcode/T658/Main.java
+++ src/main/java/com/alogrithm/leetcode/T658/Main.java
@@ -1,11 +1,43 @@
 package com.alogrithm.leetcode.T658;
 
+import java.util.*;
+
 /**
- * @description:
+ * @description: 658. 找到 K 个最接近的元素
  * @author: hanzhiming
- * @url:
+ * @url: https://leetcode-cn.com/problems/find-k-closest-elements/
  * @time: 2020/2/20 00:46
  */
 
 public class Main {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> list=new ArrayList<>();
+
+        PriorityQueue<Integer> heap=new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                Integer d1=Math.abs(o1-x);
+                Integer d2=Math.abs(o2-x);
+
+                return d2.compareTo(d1);
+            }
+        });
+
+        for (int num:arr){
+            if(heap.size()<k) heap.add(num);
+            else if(Math.abs(heap.peek()-x)>Math.abs(num-x)){
+                heap.poll();
+                heap.add(num);
+            }
+        }
+
+        while (!heap.isEmpty()){
+
+            list.add(heap.poll());
+        }
+        Collections.sort(list);
+        return list;
+    }
+
 }
--- src/main/java/com/alogrithm/leetcode/T658/Test.java
+++ src/main/java/com/alogrithm/leetcode/T658/Test.java
@@ -1,5 +1,11 @@
 package com.alogrithm.leetcode.T658;
 
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @description:
  * @author: hanzhiming
@@ -8,4 +14,17 @@ package com.alogrithm.leetcode.T658;
  */
 
 public class Test {
+
+    List<Integer> expectResult=Arrays.asList(1,2,3,4);
+
+
+    @org.junit.Test
+    public void test() {
+        Main main = new Main();
+        int []nums = {1,2,3,4,5};
+        Assert.assertEquals(expectResult,main.findClosestElements(nums,4,3));
+        Assert.assertEquals(expectResult,main.findClosestElements(nums,4,-1));
+
+    }
+
 }
