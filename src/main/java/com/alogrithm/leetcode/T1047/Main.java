10b9b02c6985ba439e08092eb8f6ebdcc5f59ef2
xiaozhi <xiaozhi@163.com>
2020-02-21 17:26:23 +0800

    leetcode 1047. 删除字符串中的所有相邻重复项

--- src/main/java/com/alogrithm/leetcode/T1047/Main.java
+++ src/main/java/com/alogrithm/leetcode/T1047/Main.java
@@ -1,11 +1,33 @@
 package com.alogrithm.leetcode.T1047;
 
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 1047. 删除字符串中的所有相邻重复项
  * @author: hanzhiming
- * @url:
+ * @url: https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/
  * @time: 2020/2/20 23:16
  */
 
 public class Main {
+
+    public String removeDuplicates(String S) {
+
+        Stack<Character> stack=new Stack<>();
+        StringBuffer sb=new StringBuffer();
+        char[] chars=S.toCharArray();
+        for(Character c:chars){
+
+            if(stack.isEmpty()) stack.push(c);
+            else if(stack.peek()==c){
+                stack.pop();
+            }else {
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) sb.append(stack.pop());
+
+        return sb.reverse().toString();
+    }
 }
--- src/main/java/com/alogrithm/leetcode/T1047/Test.java
+++ src/main/java/com/alogrithm/leetcode/T1047/Test.java
@@ -1,5 +1,7 @@
 package com.alogrithm.leetcode.T1047;
 
+import org.junit.Assert;
+
 /**
  * @description:
  * @author: hanzhiming
@@ -8,4 +10,12 @@ package com.alogrithm.leetcode.T1047;
  */
 
 public class Test {
+
+    @org.junit.Test
+    public void test() {
+        Main main = new Main();
+        Assert.assertEquals("ca",main.removeDuplicates("abbaca"));
+    }
+
+
 }
