21ea9c4ae88be09d724abe84c360a48c45ff7ea4
xiaozhi <xiaozhi@163.com>
2020-02-21 17:28:34 +0800

    leetcode 682. 棒球比赛

--- src/main/java/com/alogrithm/leetcode/T682/Main.java
+++ src/main/java/com/alogrithm/leetcode/T682/Main.java
@@ -1,11 +1,39 @@
 package com.alogrithm.leetcode.T682;
 
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 682. 棒球比赛
  * @author: hanzhiming
- * @url:
+ * @url: https://leetcode-cn.com/problems/baseball-game/
  * @time: 2020/2/20 23:02
  */
 
 public class Main {
+
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack=new Stack<>();
+        for(String str:ops){
+            if(str=="C") stack.pop();
+            else if(str=="D"){
+                int score=stack.peek();
+                stack.push(2*score);
+            }else if(str=="+"){
+                int score1=stack.pop();
+                int score2=stack.pop();
+                stack.push(score2);
+                stack.push(score1);
+                stack.push(score1+score2);
+            }else {
+                int score=Integer.parseInt(str);
+                stack.push(score);
+            }
+        }
+
+        int sum=0;
+        while (!stack.isEmpty()){
+            sum+=stack.pop();
+        }
+        return sum;
+    }
 }
--- src/main/java/com/alogrithm/leetcode/T682/Test.java
+++ src/main/java/com/alogrithm/leetcode/T682/Test.java
@@ -1,5 +1,7 @@
 package com.alogrithm.leetcode.T682;
 
+import org.junit.Assert;
+
 /**
  * @description:
  * @author: hanzhiming
@@ -8,4 +10,19 @@ package com.alogrithm.leetcode.T682;
  */
 
 public class Test {
+
+    @org.junit.Test
+    public void test1() {
+        Main main = new Main();
+        String []ops = {"5","2","C","D","+"};
+        Assert.assertEquals(30,main.calPoints(ops));
+    }
+
+    @org.junit.Test
+    public void test2() {
+        Main main = new Main();
+        String []ops = {"5","-2","4","C","D","9","+","+"};
+        Assert.assertEquals(27,main.calPoints(ops));
+    }
+
 }
