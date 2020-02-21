4984e7fb631ac774258a046091a1c0751e8a63ee
xiaozhi <xiaozhi@163.com>
2020-02-21 17:27:31 +0800

    leetcode 239. 滑动窗口最大值

--- src/main/java/com/alogrithm/leetcode/T239/Main.java
+++ src/main/java/com/alogrithm/leetcode/T239/Main.java
@@ -1,11 +1,43 @@
 package com.alogrithm.leetcode.T239;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 239. 滑动窗口最大值
  * @author: hanzhiming
- * @url:
+ * @url: https://leetcode-cn.com/problems/sliding-window-maximum/
  * @time: 2020/2/20 21:59
  */
 
 public class Main {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len=k<=0?0:nums.length-k+1;
+        int []max=new int[len];
+        Stack<Integer> stack=new Stack<>();
+        PriorityQueue<Integer> heap=new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        int left=-1;
+        for(int i=0;i<nums.length;i++){
+            if(i>=k){
+                max[left]=heap.peek();
+                heap.remove(stack.pop());
+            }
+            if(stack.isEmpty()){
+                left++;
+                stack.push(nums[left]);
+            }
+            heap.offer(nums[i]);
+            if(i==nums.length-1)  max[left]=heap.peek();
+        }
+        return max;
+    }
+
 }
--- src/main/java/com/alogrithm/leetcode/T239/Test.java
+++ src/main/java/com/alogrithm/leetcode/T239/Test.java
@@ -1,5 +1,7 @@
 package com.alogrithm.leetcode.T239;
 
+import org.junit.Assert;
+
 /**
  * @description:
  * @author: hanzhiming
@@ -8,4 +10,19 @@ package com.alogrithm.leetcode.T239;
  */
 
 public class Test {
+
+    @org.junit.Test
+    public void test() {
+
+        Main main=new Main();
+        int[] nums={5,5,5,1,1,1};
+        print(main.maxSlidingWindow(nums,0));
+    }
+
+    public void print(int []nums){
+        for(int num:nums){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
 }
