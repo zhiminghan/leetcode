44d66aac75de40c9d0aa11bc09f01b30c304a904
xiaozhi <xiaozhi@163.com>
2020-02-22 17:01:25 +0800

    leetcode 71. 简化路径

--- src/main/java/com/alogrithm/leetcode/T71/Main.java
+++ src/main/java/com/alogrithm/leetcode/T71/Main.java
@@ -1,11 +1,66 @@
 package com.alogrithm.leetcode.T71;
 
+import java.util.Stack;
+
 /**
- * @description:
+ * @description: 71. 简化路径
  * @author: hanzhiming
- * @url:
+ * @url: https://leetcode-cn.com/problems/simplify-path/
  * @time: 2020/2/21 20:58
  */
 
 public class Main {
+
+    public String simplifyPath(String path) {
+
+        char[] chars=path.toCharArray();
+        Stack<String> stack=new Stack<>();
+        StringBuffer  sb=new StringBuffer();
+
+        boolean flag=false;
+        for(char c:chars){
+            if(c=='/'&&!flag){
+                String dir=sb.toString();
+                if(dir.equals("..")&&!stack.isEmpty()) stack.pop();
+                else if (dir.length()!=0&&!dir.equals(".")&&!dir.equals(".."))stack.push(dir.toString());
+                flag=true;
+                sb=new StringBuffer();
+            }else if(c!='/'){
+                flag=false;
+                sb.append(c);
+            }
+        }
+        String dir=sb.toString();
+        if(dir.equals("..")&&!stack.isEmpty()) stack.pop();
+        else if (dir.length()!=0&&!dir.equals(".")&&!dir.equals(".."))stack.push(dir.toString());
+
+        if(stack.isEmpty()) return "/";
+        StringBuffer simdir=new StringBuffer();
+        while (!stack.isEmpty()){
+            simdir.append("/");
+            simdir.append(stack.remove(0));
+        }
+
+        return simdir.toString();
+    }
+
+    public String simplifyPath2(String path) {
+
+        Stack<String> stack=new Stack<>();
+        String[] dirs=path.split("/");
+        StringBuffer simdir=new StringBuffer();
+        for(String dir:dirs) {
+             if(dir.equals("..")&&!stack.isEmpty()) stack.pop();
+             else if (dir.length()!=0&&!dir.equals(".")&&!dir.equals(".."))stack.push(dir);
+        }
+        if(stack.isEmpty()) return "/";
+        while (!stack.isEmpty()){
+            simdir.append("/");
+            simdir.append(stack.remove(0));
+        }
+        return simdir.toString();
+    }
+
+
+
 }
--- src/main/java/com/alogrithm/leetcode/T71/Test.java
+++ src/main/java/com/alogrithm/leetcode/T71/Test.java
@@ -1,5 +1,7 @@
 package com.alogrithm.leetcode.T71;
 
+import org.junit.Assert;
+
 /**
  * @description:
  * @author: hanzhiming
@@ -8,4 +10,23 @@ package com.alogrithm.leetcode.T71;
  */
 
 public class Test {
+
+
+    @org.junit.Test
+    public void test() {
+
+        Main main = new Main();
+        Assert.assertEquals("/home", main.simplifyPath("/home/"));
+        Assert.assertEquals("/", main.simplifyPath("/../"));
+        Assert.assertEquals("/home/foo", main.simplifyPath("/home//foo/"));
+        Assert.assertEquals("/c", main.simplifyPath("/a/./b/../../c/"));
+        Assert.assertEquals("/c", main.simplifyPath("/a/../../b/../c//.//"));
+        Assert.assertEquals("/a/b/c", main.simplifyPath("/a//b////c/d//././/.."));
+
+        String[] paths="/a//b////c/d//././/..".split("/");
+        for (String path:paths) System.out.print(path+" ");
+
+    }
+
+
 }
